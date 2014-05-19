8cdc2232b8ffe554b8bcebbd9724d03a8b4609e7
sapsaldog <sapsaldog@stackmobile.kr>
2014-05-19 17:52:36 +0900

    issue #2177 resolved header parsing test code

--- Qualia/src/test/java/ItkParseTest.java
+++ Qualia/src/test/java/ItkParseTest.java
@@ -1,4 +1,8 @@
 import ITKTest.DicomImages;
+import org.itk.itkcommon.itkImageSS3;
+import org.itk.itkcommon.itkMetaDataObjectBase;
+import org.itk.itkcommon.itkMetaDataObjectS;
+import org.itk.itkiogdcm.itkGDCMImageIO;
 import org.junit.Test;
 
 /**
@@ -6,7 +10,7 @@ import org.junit.Test;
  */
 public class ItkParseTest {
     @Test
-    public void testParseID(){
+    public void testParseMetadata(){
         DicomImages dicomImages = new DicomImages();
 
         String currentDir = System.getProperty("user.dir");
@@ -18,5 +22,26 @@ public class ItkParseTest {
 
         assert(uIds[0].contentEquals("1.3.6.1.4.1.9328.50.3.69.22.500000512512"));
 
+        String[] names = dicomImages.getDicomNames(uIds[0]); // 첫번째 시리즈(uIds[0]) 선택
+
+        itkImageSS3 originalLungImage = dicomImages.loadDicomImages(names); // dcm파일로 부터 itkImage로 영상 로드
+        itkImageSS3 lungImage = originalLungImage;
+
+        String[] keys = originalLungImage.GetMetaDataDictionary().GetKeys(); // meta data는 dictionary 구조로 저장되어 있음, 현재 로드된 영상의 meta data에 사용된 key list 가져오기
+        for (String t : keys) {
+            String[] labelId = new String[1];
+            String value;
+
+            itkGDCMImageIO.GetLabelFromTag(t, labelId); // key를 이용하여 LabelID 가져오기
+
+            // down casting MetaDataObjectBase to MetaDataObjectS, String기반의 데이터를 가져오기 위한 방법, C pointer를 이용하여 Base형의 데이터를 String으로 변환
+            long pointer = itkMetaDataObjectBase.getCPtr(originalLungImage.GetMetaDataDictionary().Get(t)); // 포인터 가져오기
+
+            itkMetaDataObjectS metaObject = new itkMetaDataObjectS(pointer, false); // 포인터를 이용하여 생성
+
+            value = metaObject.GetMetaDataObjectValue(); // String형의 데이터 가져오기
+
+            System.out.println(t + ", \"" + labelId[0] + "\", {" + value + "}"); // t는 key 8개의 숫자로 구성된 key, labelID[0]는 key의 설명, value는 각각의 key에대한 값
+        }
     }
 }
