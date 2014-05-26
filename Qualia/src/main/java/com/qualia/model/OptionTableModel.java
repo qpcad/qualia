e10d8e0aac337fbe26f308561030222bc0a2d3ae
sapsaldog <sapsaldog@stackmobile.kr>
2014-05-26 19:02:05 +0900

    issue #2219 resolved

---
+++ Qualia/src/main/java/com/qualia/model/OptionTableModel.java
@@ -0,0 +1,70 @@
+package com.qualia.model;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.*;
+
+public class OptionTableModel extends AbstractTableModel{
+
+    HashMap<String, String> mMap = new HashMap<String, String>();
+    Vector<String> mKeyList = new Vector<String>();
+    Vector<String> mValueList = new Vector<String>();
+
+    String[] column = {
+            "Key", "Value"
+    };
+
+    public OptionTableModel(){
+        HashMap<String, String> optionMap = new HashMap<String, String>();
+        optionMap.put("k1", "v1");
+        optionMap.put("k2", "v2");
+        optionMap.put("k3", "v3");
+        optionMap.put("k4", "v4");
+        this.setOptionMap(optionMap);
+    }
+
+    public void setOptionMap(HashMap<String, String> map){
+        this.mMap = map;
+
+        mKeyList.clear();
+        for(String key : mMap.keySet()){
+            mKeyList.add(key);
+            System.out.println(key);
+        }
+
+        mValueList.clear();
+        for(String value : mMap.values()){
+            mValueList.add(value);
+            System.out.println(value);
+        }
+
+    }
+
+    @Override
+    public int getColumnCount() {
+        return column.length;
+    }
+
+    @Override
+    public String getColumnName(int i) {
+        return column[i];
+    }
+
+    @Override
+    public int getRowCount() {
+        return mKeyList.size();
+    }
+
+    @Override
+    public Object getValueAt(int row, int col) {
+        if(col==0){
+            return mKeyList.get(row);
+        }else{
+            return mValueList.get(row);
+        }
+    }
+
+    @Override
+    public boolean isCellEditable(int row, int col) {
+        return (col==1);
+    }
+}
--- Qualia/src/main/java/com/qualia/view/VtkView.java
+++ Qualia/src/main/java/com/qualia/view/VtkView.java
@@ -1,6 +1,8 @@
 package com.qualia.view;
 
 import com.qualia.model.Metadata;
+import com.qualia.model.OptionTableModel;
+import org.jdesktop.swingx.JXTable;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,22 +13,38 @@ public class VtkView  extends JDialog {
     public VtkView(Metadata target){
         mTargetMetadata = target;
 
-        getContentPane().setLayout(new GridLayout(2,2));
+
+        JPanel paneLeft = new JPanel();
+        getContentPane().add(paneLeft, BorderLayout.WEST);
+
+        JXTable optionTable = new JXTable(new OptionTableModel());
+        optionTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        optionTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        paneLeft.add(optionTable);
+
+        JPanel paneCenter = new JPanel();
+        paneCenter.setLayout(new BorderLayout());
+        getContentPane().add(paneCenter, BorderLayout.CENTER);
+
+        JPanel panelVtkRenderArea = new JPanel();
+        paneCenter.add(panelVtkRenderArea, BorderLayout.CENTER);
+
+        panelVtkRenderArea.setLayout(new GridLayout(2,2));
 
         VtkSliceRenderPanel panelTopLeft = new VtkSliceRenderPanel(target);
         panelTopLeft.setOrientation(VtkSliceRenderPanel.ORIENTATION_XY);
-        getContentPane().add(panelTopLeft);
+        panelVtkRenderArea.add(panelTopLeft);
 
         VtkVolumeRenderPanel panelTopRight = new VtkVolumeRenderPanel(target);
-        getContentPane().add(panelTopRight);
+        panelVtkRenderArea.add(panelTopRight);
 
         VtkSliceRenderPanel panelBottomLeft = new VtkSliceRenderPanel(target);
         panelBottomLeft.setOrientation(VtkSliceRenderPanel.ORIENTATION_YZ);
-        getContentPane().add(panelBottomLeft);
+        panelVtkRenderArea.add(panelBottomLeft);
 
         VtkSliceRenderPanel panelBottomRight = new VtkSliceRenderPanel(target);
         panelBottomRight.setModel(mTargetMetadata);
         panelBottomRight.setOrientation(VtkSliceRenderPanel.ORIENTATION_XZ);
-        getContentPane().add(panelBottomRight);
+        panelVtkRenderArea.add(panelBottomRight);
     }
 }
